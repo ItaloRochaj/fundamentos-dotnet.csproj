e5507ac20faac9500f988cdc2dc56e9a1f120c84
italorochaj <italorochaj@icloud.com>
2024-10-31 21:48:58 -0300

    Operadores Aritméticos e a classe Math

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using fundamentos_dotnet.Models;
+
+Calculadora calc = new Calculadora();
+//calc.Somar(10, 30);
+//calc.Subtrair(10, 50);
+//calc.Multiplicar(15, 45);
+//calc.Dividir(2, 2);
+
+//calc.Potencia(3, 3);
+calc.Seno(30);
+calc.Coseno(30);
+calc.Tangente(30);
